b521d87e3eec8c31b7bed84e48e3e3dbb100b318
Akash Pal <akash.pal95@gmail.com>
2021-05-17 11:34:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyCDLv0qu9sSb_gTnv0Wt0K5o0xnIk3vW94",
-  authDomain: "listify-akash95.firebaseapp.com",
-  projectId: "listify-akash95",
-  storageBucket: "listify-akash95.appspot.com",
-  messagingSenderId: "844878237220",
-  appId: "1:844878237220:web:5b0bf9985a6af8889092b1"
+  //Your firebase config keys
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
